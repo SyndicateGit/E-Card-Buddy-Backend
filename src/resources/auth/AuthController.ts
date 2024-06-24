b236b07d7136e5fdf2185644e890d2d8f352fa96
Raymond Zeng <shilei.zeng98@outlook.com>
2024-06-24 18:45:37 -0400

    Implement login feature

--- src/resources/auth/AuthController.ts
+++ src/resources/auth/AuthController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import UserModel from '../users/UserModel';
 import jwt from 'jsonwebtoken';
 import handleErrors from './AuthErrorHandler';
+import bcrypt from 'bcrypt';
 
 const maxAge = 3 * 24 * 60 * 60 * 1000 * 1000;
 const saltRound = 10;
@@ -11,12 +12,12 @@ const createToken = (userId) => {
 }
 
 const register = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
   try {
-    const { name, email, password } = req.body;
     const user = await UserModel.create({ name, email, password });
     const token = createToken(user._id)
     res.cookie('jwt', token, { httpOnly: false, maxAge: maxAge, signed: true});
-    res.status(201).json({ user: user._id, created: true, accessToken: token });
+    res.status(201).json({success: true, data:{user: user._id, created: true, accessToken: token }});
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
@@ -24,6 +25,27 @@ const register = async (req: Request, res: Response) => {
 }
 
 const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({email: email});
+
+    if(!user){
+      throw new Error("User not found");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+      throw new Error("Password is incorrect");
+    }
+
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: false, maxAge: maxAge, signed: true});
+    res.status(200).json({ success: true, data: {user: user._id, accessToken: token }});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
 
 }
 
